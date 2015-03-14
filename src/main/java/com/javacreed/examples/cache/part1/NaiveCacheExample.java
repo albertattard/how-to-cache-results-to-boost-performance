090309d2274f83361a25e4c8afa903526f5888b3
javacreed <albert@javacreed.com>
2015-03-14 15:07:57 +0100

    Initial Import

---
+++ src/main/java/com/javacreed/examples/cache/part1/NaiveCacheExample.java
@@ -0,0 +1,28 @@
+package com.javacreed.examples.cache.part1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NaiveCacheExample {
+
+    private final Map<Long, Long> cache = new HashMap<>();
+
+    public NaiveCacheExample() {
+        // The base case for the Fibonacci Sequence
+        cache.put(0L, 1L);
+        cache.put(1L, 1L);
+    }
+
+    public Long getNumber(final long index) {
+        // Check if value is in cache
+        if (cache.containsKey(index)) {
+            return cache.get(index);
+        }
+
+        // Compute value and save it in cache
+        final long value = getNumber(index - 1) + getNumber(index - 2);
+        cache.put(index, value);
+        return value;
+    }
+
+}
